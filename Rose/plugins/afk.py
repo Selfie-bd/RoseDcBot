6bca2054d0763a4750f32f7d1b6b6cfb3e087eb2
Supun Maduranga <86388269+szsupunma@users.noreply.github.com>
2022-06-16 20:14:01 -0700

    fixed
    - most likely due to a circular import

--- Rose/plugins/afk.py
+++ Rose/plugins/afk.py
@@ -6,7 +6,7 @@ from pyrogram.types import Message
 
 from Rose import app
 from Rose.utils.filter_groups import afkcheacker
-from Rose.mongo.afkdb import *
+from Rose.mongo.afkdb import is_afk, add_afk, remove_afk, get_afk_users
 
 
 def get_readable_time(seconds: int) -> str:
