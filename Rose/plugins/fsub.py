30617a33ebff1b5506db12bddad62752ecd91293
Supun Maduranga <86388269+szsupunma@users.noreply.github.com>
2022-07-16 17:43:15 -0700

    Add files via upload

--- Rose/plugins/fsub.py
+++ Rose/plugins/fsub.py
@@ -1,17 +1,26 @@
+
+    
 import asyncio
 from pyrogram import Client
-from pyrogram.errors import  UserNotParticipant
+from pyrogram.errors import FloodWait, UserNotParticipant
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from config import F_SUB_CHANNEL
 
+CHANNEL_ID = F_SUB_CHANNEL
 
-async def ForceSub(bot:Client, event: Message):
+
+async def ForceSub(bot: Client, event: Message):
     try:
-       await bot.get_chat_member(F_SUB_CHANNEL, event.from_user.id)
+        await bot.get_chat_member(chat_id=(int(CHANNEL_ID) if CHANNEL_ID.startswith("-100") else CHANNEL_ID), user_id=event.from_user.id)
+    except FloodWait as e:
+        await asyncio.sleep(e.x)
+        fix_ = await ForceSub(bot, event)
+        return fix_
     except UserNotParticipant:
         try:
            gh = await bot.send_message(chat_id=event.chat.id,text=f"""
 <b>Hey</b>{event.from_user.mention},
+
     <b>You are Free user so join my creators channel before useing me !
 Click join now button and join sz channel.</b>\n<i>Don't forget to give</i><code>/start</code><i>command 
 again.</i>""",reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Join Now", url=f"htttps://t.me/szteambots")]]),disable_web_page_preview=True)
@@ -19,6 +28,11 @@ again.</i>""",reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Join Now
            await gh.delete()
            return 400
         except:
-           event.continue_propagation() 
-           return 400  
-    
+           event.continue_propagation()    
+    except FloodWait as e:
+         await asyncio.sleep(e.x)
+         fix_ = await ForceSub(bot, event)
+         return fix_
+    except Exception as err:
+        print(f"Something Went Wrong! Unable to do Force Subscribe.\nError: {err}\n\nContact Support Group: https://t.me/supunma")
+        return 200  
