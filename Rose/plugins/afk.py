1f73d0cfff77cea5a2ad7cc6c18bdaf70b96ba45
Supun Maduranga <86388269+szsupunma@users.noreply.github.com>
2022-07-18 00:10:43 -0700

    help menu

--- Rose/plugins/afk.py
+++ Rose/plugins/afk.py
@@ -253,3 +253,17 @@ async def chat_watcher_func(_, message):
             return await message.reply_text(msg, disable_web_page_preview=True)
         except:
             return
+
+__MODULE__ = "AFK"
+__HELP__ = """
+When someone mentions you in a chat, the user will be notified you are AFK. You can even provide a reason for going AFK, which will be provided to the user as well.
+
+- `/afk` - This will set you offline.
+
+- `/afk [Reason]` - This will set you offline with a reason.
+
+- `/afk [Replied to a Sticker/Photo]` - This will set you offline with an image or sticker.
+
+- `/afk [Replied to a Sticker/Photo] [Reason]` - This will set you afk with an image and reason both.
+"""
+
