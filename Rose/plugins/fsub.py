a73a238c8d5c5e8796432e680029c2f40a24d2ff
Supun Maduranga <86388269+szsupunma@users.noreply.github.com>
2022-07-19 00:24:13 -0700

    fsub channel changed

--- Rose/plugins/fsub.py
+++ Rose/plugins/fsub.py
@@ -17,7 +17,7 @@ async def ForceSub(bot: Client, event: Message):
            gh = await bot.send_message(chat_id=event.chat.id,text=f"""
 <b>Hey </b>{event.from_user.mention} !,
 <b>You are Free user so join my creators channel before useing me !Click join now button and join sz channel.</b>
-<i>Don't forget to give</i><code>/start</code><i>command again.</i>""",reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Join Now", url="https://t.me/szteambots")]]),disable_web_page_preview=True)
+<i>Don't forget to give</i><code>/start</code><i>command again.</i>""",reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Join Now ↗️", url="https://t.me/szroseupdates")]]),disable_web_page_preview=True)
            await asyncio.sleep(10)
            await gh.delete()
            return 400
