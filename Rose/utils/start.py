c06f205b533b88b3bf787b834c5801c3a6aaba2b
Selfie SD <webtamil0@gmail.com>
2022-08-06 21:03:17 +0530

    Update start.py

--- Rose/utils/start.py
+++ Rose/utils/start.py
@@ -30,11 +30,11 @@ async def get_learn(_, m: Message, help_option: str):
 the other way to use me is to write the inline query by your self
 the format should be in this arrangement
 
-`@szrosebot your whisper @username`
+`@Groupdcs your whisper @username`
 
 now I'll split out the format in 3 parts and explain every part of it
 
-1- `@szrosebot`
+1- `@Groupdcs`
 this is my username it should be at the beginning of the inline query so I'll know that you are using me and not another bot.
 
 2- `whisper message`
@@ -44,7 +44,7 @@ it is the whisper that will be sent to the target user, you need to remove your 
 you should replace this with target's username so the bot will know that the user with this username can see your whisper message.
 
 example:- 
-`@szrosebot hello this is a test @supunma`
+`@Groupdcs hello this is a test @dcbotsa`
 
 📎 the bot works in groups and the target user should be in the same group with you
 what you are waiting for?!
